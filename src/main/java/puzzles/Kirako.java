fecfc02ef14ebc8304e33eab42445a54a81ff609
Ákos Balogh <akosbalogh16@gmail.com>
2024-03-04 18:12:38 +0100

    Kirako start

--- src/main/java/puzzles/Kirako.java
+++ src/main/java/puzzles/Kirako.java
@@ -3,9 +3,24 @@ package puzzles;
 import search.AbstractState;
 import search.State;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+enum Negyzet {A, B, C};
 public class Kirako extends AbstractState {
+
+    ArrayList<Negyzet> bal;
+    ArrayList<Negyzet> jobb;
+    public Kirako(Kirako _k, boolean _balrol, boolean _alulra)
+    {
+        super(_k);
+
+    }
     @java.lang.Override
     public Iterable<State> getPossibleMoves() {
+        Set<State> moves = new HashSet<State>();
+
         return null;
     }
 
